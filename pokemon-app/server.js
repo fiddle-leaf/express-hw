628f1a28b93719d7acc8904a8acbf748252e9b89
fiddle.leaf <shanonps@icloud.com>
2023-08-22 20:56:18 -0400

    Connected database and json info in browser

--- pokemon-app/server.js
+++ pokemon-app/server.js
@@ -2,12 +2,21 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const pokemon = require("./models/pokemon");
 
 // INDUCES
 //index
 app.get("/", (req, res) => {
   res.send("Welcome to Pokemon app!");
 });
+
+app.get("/pokemon", (req, res) => {
+  res.send(pokemon);
+});
+
+// show
+
+// listening to port
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
