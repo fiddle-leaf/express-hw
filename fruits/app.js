8d06f8f8e03c406ae6b56364cedc7ddf40faa3f5
fiddle.leaf <shanonps@icloud.com>
2023-08-24 11:08:46 -0400

    Edit pages finished (using Pug views) and reconfigured folders

--- fruits/app.js
+++ fruits/app.js
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 
 require("dotenv").config();
@@ -8,8 +8,10 @@ const app = express();
 const Veggie = require("./routes/Veggie");
 const mongoose = require("mongoose");
 const Fruit = require("./routes/Fruit");
+const methodOverride = require("method-override");
 
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 app.set("view engine", "pug");
 //app.engine("jsx", require("express-react-views").createEngine());
 
@@ -32,7 +34,11 @@ app.get("/fruits", (req, res) => {
   Fruit.find({})
     .then((allFruits) => {
       console.log(allFruits);
-      res.render("fruit", { title: "Fruits", fruits: allFruits });
+      res.render("index", {
+        title: "Fruits",
+        items: allFruits,
+        target: "/fruits",
+      });
     })
     .catch((error) => console.error(error));
 });
@@ -41,22 +47,54 @@ app.get("/vegetables", (req, res) => {
   Veggie.find({})
     .then((allVeggies) => {
       console.log(allVeggies);
-      res.render("veg", { title: "Vegetables", veggies: allVeggies });
+      res.render("index", {
+        title: "Vegetables",
+        items: allVeggies,
+        target: "/vegetables",
+      });
     })
     .catch((error) => console.error(error));
 });
 
 //new
 app.get("/fruits/new", (req, res) => {
-  res.render("new_fruit");
+  res.render("new", {
+    title: "New Fruits!",
+    name: "fruits",
+    target: "/fruits",
+  });
 });
 
 app.get("/vegetables/new", (req, res) => {
-  res.render("new_veg");
+  res.render("new_item", {
+    title: "New Veggies!",
+    name: "veggies",
+    target: "/vegetables",
+  });
 });
 
 //delete
+app.delete("/vegetables/:id", (req, res) => {
+  Veggie.deleteOne({ _id: req.params.id })
+    .then((info) => {
+      console.log(info);
+      res.redirect("/vegetables");
+    })
+    .catch((error) => console.log(error));
+});
 //update
+app.put("/veggies/:id", (req, res) => {
+  if (req.body.readyToEat === "on") {
+    req.body.readyToEat = true;
+  } else {
+    req.body.readyToEat = false;
+  }
+
+  Veggie.updateOne({ _id: req.params.id }, req.body).then((info) => {
+    console.log(info);
+    res.redirect(`/vegetables/${req.params.id}`);
+  });
+});
 
 //create
 app.post("/fruits", (req, res) => {
@@ -92,12 +130,38 @@ app.post("/vegetables", (req, res) => {
 });
 
 //edit
+app.get("/fruits/:id/edit", (req, res) => {
+  Fruit.findOne({ _id: req.params.id })
+    .then((foundFruit) => {
+      //console.log(foundFruit);
+      res.render("edit", {
+        item: foundFruit,
+        name: "Fruit",
+        target: "/fruits",
+      });
+    })
+    .catch((error) => console.log(error));
+});
+
+app.get("/vegetables/:id/edit", (req, res) => {
+  Veggie.findOne({ _id: req.params.id })
+    .then((foundVeggie) => {
+      //console.log(foundVeggie);
+      res.render("edit", {
+        item: foundVeggie,
+        name: "Vegetable",
+        target: "/vegetables",
+      });
+    })
+    .catch((error) => console.log(error));
+});
 //show
 app.get("/fruits/:id", (req, res) => {
   Fruit.findOne({ _id: req.params.id })
     .then((foundFruit) => {
-      res.render("show_fruit", {
-        fruit: foundFruit,
+      res.render("show", {
+        item: foundFruit,
+        title: "Fruits",
       });
     })
     .catch((error) => {
@@ -108,8 +172,9 @@ app.get("/fruits/:id", (req, res) => {
 app.get("/vegetables/:id", (req, res) => {
   Veggie.findOne({ _id: req.params.id })
     .then((foundVeggie) => {
-      res.render("show_veg", {
-        veggie: foundVeggie,
+      res.render("show", {
+        item: foundVeggie,
+        title: "Vegetables",
       });
     })
     .catch((error) => {
