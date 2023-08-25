097fb30bc80a6ce32a09c0be0418b4408ddd6902
fiddle.leaf <shanonps@icloud.com>
2023-08-25 16:47:08 -0400

    Update with Mongodb connection

--- pokemon-app/server.js
+++ pokemon-app/server.js
@@ -1,30 +1,75 @@
-//require("dotenv").config();
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = 3000;
-const pokemon = require("./models/pokemon");
+const mongoose = require("mongoose");
+const pokemons = require("./models/pokemons");
+const Pokemon = require("./models/Pokemon");
+
+app.use(express.urlencoded({ extended: true }));
 
 //set pug as view engine
 app.set("view engine", "pug");
 
+//connecting to Mongoose
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.once("open", () => {
+  console.log("connected to mongo");
+});
+
 // INDUCES
-//index
+// index
 app.get("/", (req, res) => {
   res.send("Welcome to Pokemon app!");
 });
 
-app.get("/pokemon", (req, res) => {
-  res.render("Index", {
-    title: "Pokemon",
-    message: "See all Pokemon!",
-    pokemons: pokemon,
-  });
+app.get("/pokemons", (req, res) => {
+  Pokemon.find({})
+    .then((allPokemons) => {
+      console.log(allPokemons);
+      res.render("Index", { pokemons: allPokemons });
+    })
+    .catch((error) => console.log(error));
+});
+
+// new
+app.get("/pokemons/new", (req, res) => {
+  res.render("new");
+});
+
+// create
+app.post("/pokemons", (req, res) => {
+  console.log(req.body);
+  Pokemon.create(req.body)
+    .then((createdPokemon) => {
+      res.redirect("/pokemons");
+    })
+    .catch((error) => console.log(error));
+});
+
+app.get("/pokemons/seed", (req, res) => {
+  Pokemon.insertMany([
+    { name: "bulbasaur", img: "http://img.pokemondb.net/artwork/bulbasaur" },
+    { name: "ivysaur", img: "http://img.pokemondb.net/artwork/ivysaur" },
+    { name: "venusaur", img: "http://img.pokemondb.net/artwork/venusaur" },
+    { name: "charmander", img: "http://img.pokemondb.net/artwork/charmander" },
+    { name: "charizard", img: "http://img.pokemondb.net/artwork/charizard" },
+    { name: "squirtle", img: "http://img.pokemondb.net/artwork/squirtle" },
+    { name: "wartortle", img: "http://img.pokemondb.net/artwork/wartortle" },
+  ]);
 });
 
 // show
-app.get("/pokemon/:id", (req, res) => {
-  //console.log(pokemon);
-  res.render("show", { pokemons: pokemon, id: req.params.id });
+app.get("/pokemons/:id", (req, res) => {
+  Pokemon.findOne({ _id: req.params.id })
+    .then((foundPokemon) => {
+      res.render("show", { pokemon: foundPokemon });
+    })
+    .catch((error) => console.log(error));
 });
 
 // listening to port
